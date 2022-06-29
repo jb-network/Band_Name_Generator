a7aa9d3bdfa3a73712d8e3a0119d00d41a296d4f
jb-network <85036487+jb-network@users.noreply.github.com>
2022-06-29 13:49:28 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+# This is Day 1 of the 100 days of code Python Bootcamp
+# The goal of this short program is to create a band name based on input provided by a user
+
+print("Welcome to the band name generator\n")
+
+CityName = input("What city did you grow up in?\n")
+
+PetName = input("\nWhat is the name of your favorite pet?\n")
+
+print("Your band name is: " + CityName + " " + PetName)
